e7492c1cfd8e3d9daa267b49faa105ab107296a5
mateusC2000 <mateus.camphos2000@gmail.com>
2024-03-15 12:07:53 -0300

    Add dependent:destroy.

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Policy < ApplicationRecord
-  has_one :vehicle
-  has_one :insured
+  has_one :vehicle, dependent: :destroy
+  has_one :insured, dependent: :destroy
 end
