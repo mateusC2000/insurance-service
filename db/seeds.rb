367b7b2b868aacba4d0da211eab744bc2c31f9cc
mateusC2000 <mateus.camphos2000@gmail.com>
2024-03-26 09:27:56 -0300

    Add seeds in project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+policy = Policy.new(number: '3003110007872', emission_date: Date.parse('2024-03-17'),
+                          coverage_end_date: Date.parse('2026-03-17'))
+policy.vehicle = Vehicle.new(brand: 'McLaren', model: '765LT', year: '2020', plate: 'JOY-4213')
+policy.insured = Insured.new(name: 'Darth Vader', cpf: '206.596.120-11')
+policy.save!
+
+puts("Preenche d, id: #{policy.id}")
